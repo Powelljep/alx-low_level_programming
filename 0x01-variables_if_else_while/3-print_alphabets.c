ce64471c3288edc175f424da3bf367a60b54801a
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-20 15:01:31 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Description: Prints all lowercase and uppercase character
+ * from A-Z
+ * Return: return int value of zero on succesful compliation
+ */
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 90)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar("\n");
+}
