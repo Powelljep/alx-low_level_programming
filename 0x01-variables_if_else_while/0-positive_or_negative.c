4003b4f31209f226ae7ad5092f32a6f3b39bed76
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-19 20:35:22 -0700

    if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+#include <stdio.h>
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -15,7 +16,7 @@ int main(void)
 	}else if(n == 0){
 		printf("%d is zero",n);
 	}else{
-		printf("%d is negative",n)
+		printf("%d is negative",n);
 	}
 	return (0);
 }
