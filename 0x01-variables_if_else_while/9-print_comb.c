aedf71370b6a4ce996650ff7273d8f80f103d89b
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-21 08:28:56 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point to the program
+ *
+ * Description: a program that prints all
+ * possible combinations of single-digit numbers.
+ * Return: returns 0 if the program runs.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return 0;
+}
