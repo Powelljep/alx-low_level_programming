1c27a8c9e7f21c722e0e549ed8d3287da2a46e76
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-21 13:54:17 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ *
+ * Description: a program that prints all
+ * possible combinations of two two-digit numbers
+ * Return: returns 0 if the program runs successfully.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; i <= 57; i++)
+		{
+			if ( i >= j)
+				continue;
+			putchar(i);
+			putchar(i);
+			putchar(32);
+			putchar(i);
+			putchar(j);
+			putchar(44);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
