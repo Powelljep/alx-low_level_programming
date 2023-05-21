9fa6a00b8834ea29564c6553882c3fd9ce06108a
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-20 18:26:01 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print numbers in ascii 
+ * Return: returns 0 when the program runs successfully 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
