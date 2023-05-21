0af961f469b6a5eecbb558adda3989a9a07bc8c3
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-20 17:42:06 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 97; i <= 122 ; i++)
 	{
-            if (i==101 || i==113)
+	       	if (i==101 || i==113)
 		    continue;
-            putchar(i);
+		putchar(i);
         }
 	putchar('\n');
 	return (0);
