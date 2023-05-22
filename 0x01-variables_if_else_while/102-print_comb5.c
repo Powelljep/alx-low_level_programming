305cfc984653ddaa0311c4ff5fdc09069699ca08
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-22 13:13:46 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,32 +8,33 @@
  */
 int main(void)
 {
-       	int i, j, k, l;
+	int i, j, k, l;
+        
 	for (i = 48; i <= 57; i++)
-	{
-		for (j = 48; j <= 57; j++)
-		{
-			for (k = 48; k<= 57; k++)
-			{
-				for (l = 49; l<= 57; l++)
-				{
-					if(i==48 && j == 48 && k == 48 && l == 48)
-                                    		continue;
-					if (i == 57 && j == 57 && k == 57 && l == 57)
-						break;
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(l);
-					if (i == 57 && j == 57 &&k == 57 && l == 56)
-						continue;
-					putchar(44);
-					putchar(32);
-				}
-			}
-		}
-	}
-	putchar('\n');
+        {
+                for (j = 48; j <= 57; j++)
+                {
+                        for (k = 48; k<= 57; k++)
+                        {
+                                for (l = 48; l<= 57; l++)
+                                {
+                                    if(i==48 && j == 48 && k == 48 && l == 48)
+                                    continue;
+                                        if (i == 57 && j == 57 && k == 57 && l == 57)
+                                                break;
+                                        putchar(i);
+                                        putchar(j);
+                                        putchar(32);
+                                        putchar(k);
+                                        putchar(l);
+                                        if (i == 57 && j == 57 &&k == 57 && l == 56)
+                                                continue;
+                                        putchar(44);
+                                        putchar(32);
+                                }
+                        }
+                }
+        }
+        putchar('\n');
 	return (0);
 }
