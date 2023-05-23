f76ff4e99af259526b2bd6fe980bc3919e92f12d
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 09:01:05 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print lowercase letters
+ *
+ * Description: prints lowercase letters
+ * Return: returns 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
