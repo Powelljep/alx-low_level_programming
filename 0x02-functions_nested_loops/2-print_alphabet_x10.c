91068329024bd3e6249f33ccb72b3a2f9ba01593
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 11:57:18 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,20 @@
 /**
  * print_alphabet_x10 - prints alfabets.
  *
- * Description: prints 10 times the alphabet, in lowercase, followed by a new line
+ * Description: prints 10 times the alphabet,
+ * in lowercase, followed by a new line
  * Return: returns void
  */
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
-	}	
-	_putchar('\n');
+		_putchar(j);
+	}
 }
