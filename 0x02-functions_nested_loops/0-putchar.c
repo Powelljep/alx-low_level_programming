1397388a62150fd00c0a6072d534784dad02f773
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 06:14:54 -0700

    add file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * main - prints the word _putchar.
  *
@@ -10,4 +12,23 @@ int main(void)
 	_putchar();
 	
 	return (0);
+}
+
+/**
+ * _putchar - prints the string "_putchar".
+ *
+ * Description: the function prints the string putchar.
+ * Return: the function returns void and prints putchar on the console.
+ */
+void _putchar(void) {
+
+        putchar(95);
+        putchar(112);
+        putchar(117);
+        putchar(116);
+        putchar(99);
+        putchar(104);
+        putchar(97);
+        putchar(114);
+        putchar('\n');
 }
