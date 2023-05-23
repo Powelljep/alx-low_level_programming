7ca85c2ea85c1bfbd1010847c0a92b977b983152
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 08:01:36 -0700

    add

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point to the program
+ *
+ * Description: prints _putchar
+ * Return: returns 0
+ */
+int main(void)
+{
+	printputchar();
+	return 0;
+}
