65cdc07f7858859a5c5a6e56abbc83da7d26246f
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 05:56:12 -0700

    adding _putchar.c file

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * main - prints the word _putchar.
- *
- * Description: prints putchar.
- * Return: returns 0 if the program runs successfully.
- */
-int main(void)
-{
-	_putchar();
-	
-	return (0);
-}
-
 /**
  * _putchar - prints the string "_putchar".
  *
