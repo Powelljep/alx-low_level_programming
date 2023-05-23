d6c0df2bf7ba9ab77d6ea4635d8c7acbfc72d63f
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 14:44:46 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks alphabetic character
+ * @c: is an int value
+ *
+ * Description: a function that
+ * checks for alphabetic character
+ * Return: returns 1 if c is a letter, lowercase or uppercase
+ * returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+	char i, j;
+
+	for (i = 'A'; i <= 'Z'; i++)
+		if (c == i)
+			return (1);
+	return (0);
+}
