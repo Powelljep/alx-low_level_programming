bc0e70880aafec603f3e74e8efb979e2e8e41a24
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 08:32:50 -0700

    finf bearings

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _putchar - prints the string "_putchar".
+ * main - entry point to the program
  *
- * Description: the function prints the string putchar.
- * Return: the function returns void and prints putchar on the console.
+ * Description: prints _putchar
+ * Return: returns 0
  */
-void printputchar(void) {
-
-        putchar(95);
-        putchar(112);
-        putchar(117);
-        putchar(116);
-        putchar(99);
-        putchar(104);
-        putchar(97);
-        putchar(114);
-        putchar('\n');
+int main(void)
+{
+       	_putchar();
+	return 0;
 }
