b62442c330600202de2c1cd89e5cbdf44632e826
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 15:53:45 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,11 +15,12 @@ int print_sign(int n)
 	       return (1);
        } else if (n == 0)
        {
-	       _purchar('0');
+	       _putchar('0');
 	       return (0);
        } else
        {
-	       _putchar('-1');
+	       _putchar('-');
+	       _putchar('1');
        }
 
 }
