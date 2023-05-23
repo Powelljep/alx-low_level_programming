5f7ffea19d3463c6193bd644d5d8eb46f4393459
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 05:51:09 -0700

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,6 @@
-#include <stdio.h>
 #ifndef MAIN
 #define MAIN
-/**
- * _putchar - prints the string "_putchar".
- *
- * Description: the function prints the string putchar.
- * Return: the function returns void and prints putchar on the console.
- */
-void _putchar(void) {
 
-	putchar(95);
-        putchar(112);
-        putchar(117);
-        putchar(116);
-        putchar(99);
-        putchar(104);
-        putchar(97);
-        putchar(114);
-        putchar('\n');
-}
+void _putchar(void);
+
 #endif
