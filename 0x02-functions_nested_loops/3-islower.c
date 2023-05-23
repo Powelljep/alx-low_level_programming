7a98e7b6eb89a2715734a50d7b326f650f8f41bd
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 14:16:12 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * int - _islower - check if a letter is lowercase
+ * @c: character to be checked
+ *
+ * Description: checks for lowercase character
+ * Return: returns an int value of 1 is c is lower
+ * or 0 otherwise
+ */
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if ( c == i)
+			return 1;
+	}
+	return 0;
+}
