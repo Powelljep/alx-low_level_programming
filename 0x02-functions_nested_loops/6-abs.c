73d16832537c779520319d935e4fbcb510adbaea
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 18:36:48 -0700

    signed

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * int _abs - computes the absolute value of an integer.
  * @val: an int to return its abs value
-
+ *
  * Description: a function that computes the
  * absolute value of an integer.
  * Return: returns and in value;
@@ -11,6 +11,7 @@ int _abs(int val)
 {
 	unsigned int value;
 	int signedval = val;
+
 	value = signedval;
 	value = (signedval >= 0 ? signedval : -signedval);
 	return (value); 
