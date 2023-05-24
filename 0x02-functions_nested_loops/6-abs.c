afce595e47f13d6408428d581efd1e5d5bdd12dd
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-23 18:14:37 -0700

    signed

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,9 +9,9 @@
  */
 int _abs(int val)
 {
-	unsigned int value;
-	if (val >= 0)
-		return (val);
-	value = (unsigned int) val;
-	return (value);
+	 unsigned int value;
+	 int signedval = val;
+	 value = signedval;
+	 value = (signedval >= 0 ? signedval:-signedval);
+	return (value); 
 }
