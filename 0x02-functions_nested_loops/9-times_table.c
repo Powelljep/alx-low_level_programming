c9ddca6ed58ca85d2cef2ece377e29e851520a9f
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-24 15:46:45 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,11 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			if(j*i < 10)
+				printf(" ");
 			printf("%d", i*j);
 			if (j != 9)
 				printf(", ");
-			if (j*i < 10)
-				printf(" ");
 			if (j == 9)
 				printf("\n");
 		}
