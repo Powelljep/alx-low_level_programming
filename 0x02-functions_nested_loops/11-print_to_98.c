320c3c9c35f47e1ee8696ed002ba84dfad415c30
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-24 13:49:40 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from
+ * n to 98, followed by a new line.
+ *
+ * Description: prints all natural
+ * numbers from n to 98, followed by a new line.
+ * Return: returns void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i < 98)
+			printf(", \n");
+
+	}
+}
