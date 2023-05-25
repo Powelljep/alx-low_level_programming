c5c4afab0251b4efed2169a66747abb9af56548a
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-24 22:13:15 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+/**
+ * _isupper - checks for uppercase
+ * @c: parameter to check
+ *
+ * Description: a function that checks for uppercase character.
+ * Return: Returns 1 if c is uppercase.
+ * Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	if(isupper(c))
+		return (1);
+	return (0);
+}
