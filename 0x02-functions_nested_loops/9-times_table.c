8ce0427f303d0b15dbb76a7ed64f5f17f1714fbc
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-24 19:04:27 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,9 +16,9 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			if (i * j <= 9)
-				printf(" %3d", i * j);
+				printf(" %2d", i * j);
 			else
-				printf("%3d", i * j);
+				printf("%2d", i * j);
 			if (j < 9)
 				printf(",");
 			if (j == 9)
