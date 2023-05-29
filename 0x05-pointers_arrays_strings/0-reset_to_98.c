d8f216cc95c4340d911f3788ed97d13d5f416c21
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-29 13:46:17 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 -  updates the value it points to to 98
- * n: pointer param
+ * @n: pointer param
  *
  * Description: a function that takes a pointer to an
  * int as parameter and updates the value it points to to 98.
