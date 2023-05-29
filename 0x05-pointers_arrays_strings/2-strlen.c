3167c7cb927e90df5c9a370b1016f7128d987dec
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-29 14:06:53 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to return the length of.
+ *
+ * Description: a function that returns the length of a string.
+ * Return: int length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
