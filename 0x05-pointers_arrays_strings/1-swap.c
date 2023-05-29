b5ba22b00ad5d4407cd00f0d02d671708b97cddd
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-29 13:54:03 -0700

    swap variable values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first value to be swapped with the second value.
+ * @b: second value to be swapped with the first value.
+ *
+ * Description: a function that swaps the values of two integers.
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap
+}
