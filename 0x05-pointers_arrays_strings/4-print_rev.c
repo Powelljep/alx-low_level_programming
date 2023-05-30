202cd8e2fd691dc03bdca27b1561ac0a3d49a6b1
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-30 16:55:04 -0700

    a function that returns the length of a string.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,9 +16,9 @@ void print_rev(char *s)
 		len++;
 	}
 
-	counter = len - 2;
+	counter = len;
 
-	while (*(s + counter) >= 0)
+	while (*(s + counter) > 0)
 	{
 		_putchar(*(s + counter));
 		counter--;
