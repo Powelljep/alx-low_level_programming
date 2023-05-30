092285db76a66dd619157058b97265cbf6bf8607
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-30 16:14:15 -0700

    a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - that prints a string, in reverse
+ * @s: string to print
+ *
+ * Description: a function that prints a string,
+ * in reverse, followed by a new line.
+ * Return:  void
+ */
+void print_rev(char *s)
+{
+	int counter = _strlen(*s);
+
+	while (*(s+counter) >= 0)
+	{
+		_putchar(*(s + counter));
+		counter--;
+	}
+	_putchar('\n');
+}
