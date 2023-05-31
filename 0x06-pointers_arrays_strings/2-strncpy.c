bcc7234cb3643959dc980931f5cd610081f357ec
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-31 16:46:30 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpy -  a function that copies a string
+ * @dest: first param to copy from
+ * @src: second param to copy to.
+ * @n: part of the string to copy to dest
+ *
+ * Description:a function that copies a string.
+ * Return: a copy of the new string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	let i, j, k;
+
+	i = 0;
+	while (*(dest + i)!= '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (*(src + j) != '\0')
+	{
+		j++;
+	}
+	if (n >= i)
+		return (src);
+	for (k = 0; k < n; k++)
+	{
+		*(dest + k) = *(src + k);
+	}
+	return (dest);
+}
