8ffe540fb6acbbb71a83dd40935a883cdeb1b0cf
John Powell Odhiambo <powell.john2011@gmail.com>
2023-05-31 15:31:02 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: first param
+ * @src: second param
+ * @n: third param
+ *
+ * Description: a function that concatenates two strings
+ * Return: returns the concatenated string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len;
+	
+	i = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (*(src + j) != '\0')
+	{
+		j++;
+	}
+	
+	for (len = 0; len < j; len++)
+	{
+		if (len < n + 1)
+		{
+			*(dest + i + len) = *(src + len);
+		}
+	}
+	return (dest);
+}
