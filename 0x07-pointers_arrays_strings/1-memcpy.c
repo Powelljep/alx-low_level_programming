70081fad1754ad01d6422298e886fcb61e262098
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-05 07:33:09 -0700

    new changes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @dest:destination to copy  bytes from memory area
+ * @src: memory area to copy from.
+ * @n:number of  bytes from memory area to copy.
+ *
+ * Description:a function that copies memory area.
+ * Return: Returns a pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+}
