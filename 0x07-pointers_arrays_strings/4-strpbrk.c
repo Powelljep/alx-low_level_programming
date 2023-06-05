b29f25d391f102133939db3bf99facfb06806b32
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-05 08:08:44 -0700

    function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strpbrk -  searches a string for any of a set of bytes
+ * @s:
+ * @accept:
+ *
+ * Description: 
+ * Return:
+ */
+char *_strpbrk(char *s, char *accept)
+{
+}
