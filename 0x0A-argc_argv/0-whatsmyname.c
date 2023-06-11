1599557a25e699daadbd46c87c1afe5b8986cde4
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-10 23:08:01 -0700

    corr

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * Description: a program that prints its name, followed by a new line.
  * Return: returns 0 if the program runs successfully.
  */
-int main(int argc, char[] argv)
+int main(int argc, char *argv[])
 {
-	printf("%s \n",argv[0]);
+	printf("%s\n",argv[0]);
 	return (0);
 }
