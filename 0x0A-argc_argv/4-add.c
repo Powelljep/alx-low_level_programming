784eabf03b18fec5b20f59c2a5b9ed226e5329c6
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-14 20:01:42 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive numbers
+ * @argc - count of params
+ * @argv - array of param strings
+ *
+ * Description: a program that adds positive numbers.
+ * Return: returns 0 if successful or 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (arg < 2)
+	{
+		printf("%d\n", sum);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		if (!isdigit(atoi(*(argv + i))))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum *= atoi(argv[i]);
+	}
+	prinf("%d\n", sum);
+	return (0);
+}
