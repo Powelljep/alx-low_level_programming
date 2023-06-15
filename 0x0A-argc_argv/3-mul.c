6a2cdbe42359795cf4c92640b5b6aa0472c07604
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-14 19:41:14 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	num1 = ato(argv[1]);
+	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	sum = num1 * num2;
 	printf("%d\n", sum);
