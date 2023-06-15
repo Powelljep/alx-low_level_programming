a2ddd9652fe5c2a5f0b62d4eac21c93a0de630fb
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-14 18:03:20 -0700

    add main.c

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints
+ *
+ * Return: int
+ */
+int main(void)
+{
+    puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
