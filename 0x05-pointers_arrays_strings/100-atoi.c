1bc622015db64b82bf17d7806b68a8f87d697afb
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-16 13:09:00 -0700

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _atoi -  convert a string to an integer.
+ * @: string to convert to interger
+ *
+ * Description: a function that convert a string to an integer.
+ * Return: returns int.
+ */
+int _atoi(char *s)
+{
+}
