144b6823c4170bde1b6275904922bc150fa84dc9
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-17 05:00:26 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints exactly with proper grammar, but the outcome is a piece of art
+ *
+ * Description: a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ * Return: nothing
+ */
+int maint(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
