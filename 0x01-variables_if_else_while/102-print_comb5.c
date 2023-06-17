981b7a13a5e1e80fc56995f88dfb97fd2d7545d9
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-17 01:59:56 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,33 +9,25 @@
 int main(void)
 {
 	int i, j, k, l;
-        for (i = 48; i <= 57; i++)
-        {
-                for (j = 48; j <= 57; j++)
-                {
-                        for (k = 48; k<= 57; k++)
-                        {
-                                for (l = 48; l<= 57; l++)
-                                {
-                                    if (i == 57 && j == 57 && k == 57 && l == 57)
-					    break;
-				    if (i == k && j == l)
-					    continue;
-				    if (k == 48 && l == 48)
-					    continue;
-                                   	putchar(i);
-                                        putchar(j);
-                                        putchar(32);
-                                        putchar(k);
-                                        putchar(l);
-                                        if (i == 57 && j == 57 &&k == 57 && l == 56)
-						continue;
-                                        putchar(44);
-                                        putchar(32);
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 59; k++)
+			{
+				for (l = 49; l < 59; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					
+					if (i < 58 && j < 57 && k < 58 && l < 58)
+						putchar(44);
+				}
+			}
+		}
+	}
 }
