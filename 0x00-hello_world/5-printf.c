8be5a2c171e50cbeb8f9af202151dfb9618d7b52
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-17 05:01:27 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * but the outcome is a piece of art,, followed by a new line.
  * Return: nothing
  */
-int maint(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
