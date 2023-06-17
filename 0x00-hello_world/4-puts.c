e146233e72691b15c602ed50d32dddf96a5c34f6
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-17 04:52:27 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle"
+ *
+ * Description: a C program that prints exactly "Programming is
+ * like building a multilingual puzzle, followed by a new line.
+ * Return: nothing
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\0");
+	return (0);
+}
