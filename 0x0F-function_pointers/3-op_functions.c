9e38938fde316a5d9175ad0096ce4d94226b71c9
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-21 12:32:32 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,44 @@
+/**
+ * op_add -
+ *
+ * Return:
+ */
+int op_add(int a, int b)
+{
+}
+
+/**
+ * op_sub -
+ *
+ * Return:
+ */
+int op_sub(int a, int b)
+{
+}
+
+/**
+ * op_mul -
+ *
+ * Return:
+ */
+int op_mul(int a, int b)
+{
+}
+
+/**
+ * op_div -
+ *
+ * Return:
+ */
+int op_div(int a, int b)
+{
+}
+
+/**
+ * op_mod -
+ *
+ * Return:
+ */
+int op_mod(int a, int b)
+{
+}
