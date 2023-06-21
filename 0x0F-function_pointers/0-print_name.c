3d8ff987c60d30a2144fa32a3ce2c2ffc9e74a14
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-20 22:32:07 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name : name of the person
+ * @f : pointer funct
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(*name);
+
+}
