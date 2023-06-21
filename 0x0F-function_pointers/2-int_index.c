ba0bc176583612a22742429b344b605c475e3dc7
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-21 09:51:12 -0700

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int int_index - a function that searches for an integer.
+ * @array: array to search from 
+ * @size: size of the array
+ * @cmp: pointer funtion to do the search
+ *
+ * Return: returns int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)) == 0)
+			return (i);
+	}
+	return (-1);
+}
