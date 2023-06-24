bd5b0030bdadc0f7e34e3cab1626be499bbbc4c5
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-24 06:54:54 -0700

    4. A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - a function that creates a new dog.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * 
+ * Description: a function that creates a new dog.
+ * Return: dog_t or null 
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog;
+
+	if ( name == NULL || age == NULL || owner == NULL)
+		return (NULL);
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+	return (newdog);
+
+}
