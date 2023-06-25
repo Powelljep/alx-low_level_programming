ea4651bf5d322f45c414124b1a997243fc8173cf
John Powell Odhiambo <powell.john2011@gmail.com>
2023-06-25 13:38:26 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: string to print
+ *
+ * Description: recursive function that prints a string
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	
+	if (*(s + i) == '\0')
+		return;
+	_putchar(*(s + i));
+	i++;
+	_puts_recursion((s + i));
+
+}
