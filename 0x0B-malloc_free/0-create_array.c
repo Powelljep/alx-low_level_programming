bdd3bfd89cc28486c094fc9c255b8a55d5539019
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-10 07:25:04 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,10 +17,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = (char *) malloc(sizeof(char) * size);
-
-	for (i = 0; i < size; i++)
+	if (p != NULL)
 	{
-		*(p + i) = c;
+		for (i = 0; i < size; i++)
+		{
+			*(p + i) = c;
+		}
+		return (p);
 	}
-	return (p);
+	return (NULL);
 }
