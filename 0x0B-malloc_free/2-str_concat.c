bd3a956c6a4d68a8f083ceee8fede6470ef21930
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-10 08:27:54 -0700

    for (l = 0; l == i; l++)         {                 *(p + l) = *(s1 + l);
    exit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat -  concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Description: The returned pointer should point to a newly allocated
+ * space in memory which contains the contents of s1, followed by the
+ * contents of s2, and null terminated
+ * Return: return NULL on failure or pointer to the newly created
+ * string.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0; j = 0; k, l, m;
+	char *p;
+
+	while (*(s1 + i) != '\0')
+	{
+		i++;
+	}
+	while (*(s2 + j) != '\0')
+	{
+		j++;
+	}
+	i++;
+	k = i + j;
+	p = malloc(sizeof(char) * k);
+	if (p == NULL)
+		return NULL;
+	for (l = 0; l < i; l++)
+	{
+		*(p + l) = *(s1 + l);
+	}
+	j++
+	 for (m = 0; m < j; j++)
+        {
+                *(p + i++) = *(s2 + l);
+        }
+	 return (p);
+}
