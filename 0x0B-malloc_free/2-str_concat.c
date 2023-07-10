e6705899f253449fd286548e4640ffebd4629014
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-10 12:12:44 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,34 +13,43 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, strsize = 0, k = 0, l = 0;
-	char *p;
-
-	while (*(s1 + i) != '\0')
-	{
-		i++;
-	}
-	while (*(s2 + j) != '\0')
-	{
-		j++;
-	}
-	strsize = i + j + 1;
-	p = malloc(sizeof(char) * strsize);
-	if (p == NULL)
-		return (NULL);
-
-	while (*(s1 + k) != '\0')
-	{
-		*(p + k) = *(s1 + k);
-		k++;
-	}
-	while (*(s2 + l) != '\0')
-	{
-		*(p + i) = *(s2 + l);
-		i++;
-		l++;
-	}
-	*(p + strsize) = '\0';
-	return (p);
+        int i = 0, j = 0, strsize = 0, k = 0, l = 0;
+        char *p;
+        
+        if (s1 != NULL)
+        {
+            while (*(s1 + i) != '\0')
+            {
+                    i++;
+            }
+        }
+        if (s2 != NULL)
+        {
+            while (*(s2 + j) != '\0')
+            {
+                j++;
+        }
+        
+        }
+        strsize = i + j + 1;
+        p = malloc(sizeof(char) * strsize);
+        if (p == NULL)
+                return (NULL);
+        if (s1 != NULL)
+        {
+        while (*(s1 + k) != '\0')
+        {
+                *(p + k) = *(s1 + k);
+                k++;
+        }}
+        if (s2 != NULL){
+        while (*(s2 + l) != '\0')
+        {
+                *(p + i) = *(s2 + l);
+                i++;
+                l++;
+        }}
+        *(p + strsize) = '\0';
+        return (p);
 
 }
