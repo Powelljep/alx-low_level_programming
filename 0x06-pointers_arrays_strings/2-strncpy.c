92c114edc91ee1365ddab99024b42a88d30bf54a
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-11 04:06:11 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,14 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (*(src + i) != '\0')
-	{
-		i++;
-	}
-	for (j = 0; j < n; j++)
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		*(dest + j) = *(src + j);
 	}
+	for (; j < n; j++)
+	       *(dest + j) = '\0';	
 
 	return (dest);
 }
