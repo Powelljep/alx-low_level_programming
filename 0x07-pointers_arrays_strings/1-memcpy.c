5afb9bbea33da368039604a7ed9fc35a7405f2b5
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-18 09:27:27 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	while (*(src + i) != '\0')
+	while (*(src + i) != '\0' && i < n)
 	{
 		*(dest + i) = *(src + i);
+		i++;
 	}
 	return (dest);
 }
