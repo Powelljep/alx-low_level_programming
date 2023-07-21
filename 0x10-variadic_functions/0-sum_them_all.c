8071c3ac63e7a9b0422ca70c91e36dbb11c3cd79
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-21 05:34:01 -0700

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,11 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list params;
 	int i, sum = 0;
+
 	va_start(params,n);
 
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(params,int);
 	}
+	va_end(params);
 	return (sum);
 }
