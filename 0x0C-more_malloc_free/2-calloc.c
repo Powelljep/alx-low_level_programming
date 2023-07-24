0ca3f28558a36c4ad4314f61852a6a4b4ad4baf4
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-24 04:06:31 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(sizeof(size) * nmemb);
+	p = malloc(size * nmemb);
 	if (!p)
 		return (NULL);
 	while (i < nmemb)
