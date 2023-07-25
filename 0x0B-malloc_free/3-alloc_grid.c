876625924ea5a7ab23e9b2010ca02742e7824c08
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-25 12:08:04 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,39 +1,39 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * alloc_grid -  returns a pointer to a 2 dimensional array of integers.
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
  * @width: width of the array
  * @height: height of the array
  *
- * Return:  returns a pointer to a 2 dimensional array of integers.
+ * Return: returns a pointer to a 2 dimensional array of integers.
  */
 int **alloc_grid(int width, int height)
 {
-	int **ptp, *p, i = 0, j = 0;
+    if (width <= 0 || height <= 0)
+        return NULL;
 
-	ptp = malloc(sizeof(int *) * height);
-	if (!ptp)
-		return (NULL);
-	while (i < height)
-	{
-		p = malloc(sizeof(int *) * width);
-		if (!p)
-		{
-			while(j < height * width)
-			{
-				free((p + j));
-				j++;
-			}
-			return (NULL);
-		}
-		while (j < width)
-		{
-			*(p + j) = 0;
-			j++;
-		}
-		ptp = &p;
-		p++;
-		ptp++;
-	}
-	return (ptp);
+    int **ptp = malloc(sizeof(int *) * height);
+    if (!ptp)
+        return NULL;
+
+    for (int i = 0; i < height; i++)
+    {
+        ptp[i] = malloc(sizeof(int) * width);
+        if (!ptp[i])
+        {
+            // Free previously allocated memory before returning NULL
+            for (int j = 0; j < i; j++)
+                free(ptp[j]);
+            free(ptp);
+            return NULL;
+        }
+
+        for (int j = 0; j < width; j++)
+        {
+            ptp[i][j] = 0;
+        }
+    }
+
+    return ptp;
 }
