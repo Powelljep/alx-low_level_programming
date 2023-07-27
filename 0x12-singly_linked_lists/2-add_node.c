270412c45be5e4cc15d3ae06f132c3ca9daa9958
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-27 15:52:38 -0700

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode = malloc(sizeof(list_t));
 
-	if (str == NULL)
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+	if (newnode->str == NULL)
 	{
 		free(newnode);
 		return (NULL);
 	}
-	newnode->str = strdup(str);
 	newnode->len = (unsigned int) strlen(str);
 	newnode->next = *head;
 	*head = newnode;
