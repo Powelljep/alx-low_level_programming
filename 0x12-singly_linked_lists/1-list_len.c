563517cc7d27129de5b28a9d84b98802fbdee56e
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-27 13:53:34 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to linkedlist nodes.
+ *
+ * Return: returns the number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t lenofh = 0;
+
+	while (h != NULL)
+	{
+		lenofh++;
+	}
+
+	return (lenofh);
+}
