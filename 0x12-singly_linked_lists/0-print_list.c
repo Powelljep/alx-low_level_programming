cf042735d64e19c41e9cc0a5de210f2fa1300c8f
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-27 12:39:29 -0700

    adding header files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: linked list pointer
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+
+}
