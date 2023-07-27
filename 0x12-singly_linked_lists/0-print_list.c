c676e631ae09bed084437868328936bdb6b6d344
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-27 13:09:05 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <lists.h>
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: linked list pointer
@@ -7,5 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	size_t numofnodes = 0;
+	unsigned int i = 0;
 
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("%c", h->str);
+		numofnode++;
+		h->next;
+	}
+	return (numofnodes);
 }
