cda96758fadb924ad397525388aa472ea765943d
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-29 10:32:15 -0700

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list.
+ * @head: list to be freed
+ *
+ * Return: returns nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *p = head;
+
+	while(p
+}
