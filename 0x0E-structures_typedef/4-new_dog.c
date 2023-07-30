3353deb4ab4c5a7419b77f892d04e31b5ed34d9a
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-30 05:09:19 -0700

    4. _realloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,12 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = strdup(name);
-	if (new->name == NULL)
-	{
-		free(newdog);
-		return (NULL);
-	}
+	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
 	return (newdog);
