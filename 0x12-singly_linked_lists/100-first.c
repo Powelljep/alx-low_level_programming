f318299792b1f55af62eb213d86f78a5d9e33145
John Powell Odhiambo <powell.john2011@gmail.com>
2023-07-31 17:11:39 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -17,6 +17,6 @@ void print_before_main(void)
  */
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
