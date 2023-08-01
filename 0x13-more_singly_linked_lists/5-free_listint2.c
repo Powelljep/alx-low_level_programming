d6508e155db7f3f0055c7bf10adb7ec69b015e4c
John Powell Odhiambo <powell.john2011@gmail.com>
2023-08-01 08:09:14 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: pointer to head node pointer
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p, *temp;
+
+	p = *head;
+	while (p->next != NULL)
+	{
+		temp = p->next;
+		free(p);
+		p = temp;
+	}
+	free(p);
+	head = NULL;
+}
