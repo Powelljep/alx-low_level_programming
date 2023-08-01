f375aa8174549ff8c2f09df89d587ea74c797fb3
John Powell Odhiambo <powell.john2011@gmail.com>
2023-08-01 11:58:06 -0700

    6. Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,15 +10,13 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *p, *temp;
+	listint_t *temp;
 	int n = 0;
 
 	if (head == NULL)
 		return (n);
-	p = *head;
-	n = p->n;
-	temp = p->next;
-	p = NULL;
+	n = *head->n;
+	temp = *head->next;
 	free(*head);
 	*head = temp;
 	return (n);
