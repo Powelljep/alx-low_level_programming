627822eed25c3d394517445481ba0ee096b60388
John Powell Odhiambo <powell.john2011@gmail.com>
2023-08-01 22:24:26 -0700

    vi 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,44 +1,59 @@
 /**
- * op_add -
+ * op_add - performs addition
+ * @a: first operand
+ * @b: second operand
  *
- * Return:
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
+	return (a + b);
 }
 
 /**
- * op_sub -
+ * op_sub - performs subtraction
+ * @a: first operand
+ * @b: second operand
  *
- * Return:
+ * Return: subtraction of b from a
  */
 int op_sub(int a, int b)
 {
+	return (a - b);
 }
 
 /**
- * op_mul -
+ * op_mul - performs multiplication on operands
+ * @a: first operand
+ * @b: second operand
  *
- * Return:
+ * Return: multipication of a by b
  */
 int op_mul(int a, int b)
 {
+	return (a * b);
 }
 
 /**
- * op_div -
+ * op_div - divides operands
+ * @a: first operand
+ * @b: second operand
  *
- * Return:
+ * Return: division of a and b
  */
 int op_div(int a, int b)
 {
+	return (a / b);
 }
 
 /**
- * op_mod -
+ * op_mod - performs modulo operation on operands
+ * @a: first operand
+ * @a: first operand
  *
- * Return:
+ * Return: modulus of a and b
  */
 int op_mod(int a, int b)
 {
+	return (a % b);
 }
