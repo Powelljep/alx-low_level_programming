5adc183556ebd63884bc20cdfc27e79798bceef7
John Powell Odhiambo <powell.john2011@gmail.com>
2023-08-01 23:08:45 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,13 +5,18 @@
  * @size: size of the array
  * @cmp: pointer funtion to do the search
  *
- * Return: returns int
+ * Return: returns the index of the first element
+ * for which the cmp function does not return 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || !array || !cmp)
+	if (size <= 0)
+		return (-1);
+	if (!array)
+		return (-1);
+	if (!cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
