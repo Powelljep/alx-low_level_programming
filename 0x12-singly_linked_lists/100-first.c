fee951063b2e695f922e5b80a709db00328bb37d
John Powell Odhiambo <powell.john2011@gmail.com>
2023-08-02 16:05:18 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void print_before_main(void) __attribute__((constructor));
 /**
  * print_before_main - prints You're beat! and yet, you must allow,
  * \nI bore my house upon my back!\n before the main function is executed.
