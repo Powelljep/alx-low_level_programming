f84355fd129397adc0ba8544083370036ba561da
John Powell Odhiambo <powell.john2011@gmail.com>
2023-11-13 11:58:30 -0800

    locate char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,7 @@
  * @c: character to locate.
  *
  * Description: a function that locates a character in a string.
- * Return: Returns a pointer to the first occurrence of the 
+ * Return: Returns a pointer to the first occurrence of the
  * character c in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
